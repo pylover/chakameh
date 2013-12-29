13b420e688a6d307b36942749647b644d2ef8507
pylover <vahid.mardani@gmail.com>
2013-12-29 19:52:14 +0330

    idexer

--- source/chakameh/indexer.py
+++ source/chakameh/indexer.py
@@ -4,60 +4,23 @@ Created on:    Dec 29, 2013
 @author:        vahid
 '''
 
-import id3reader
+
 import os
+from model import Track,FileExistsException,setup as setup_model
 
-class MP3Reader(id3reader.Reader):
-    def __init__(self,f):
-        id3reader.Reader.__init__(self, f)
+def import_file(filename):
+    pass
     
-    @property
-    def title(self):
-        return self.getValue('title')
-    
-    @property
-    def album(self):
-        return self.getValue('album')
-
-    @property
-    def artist(self):
-        return self.getValue('performer')
-
-    @property
-    def composer(self):
-        return self.getValue('composer')
-    
-    @property
-    def year(self):
-        return self.getValue('year')
-    
-    @property
-    def lyricist(self):
-        return self.getValue('TEXT')
-    
-    @property
-    def genere(self):
-        return self.getValue('genere')    
-    
-#     def __repr__(self):
-# #         if self.frames:
-# #             return str(self.frames.keys())
-# #         else:
-# #             return '\n'
-#         return u'%s\t%s\t%s\t%s\t%s\t%s\t%s' % (self.album,self.artist,self.composer,self.year,self.title,self.lyricist,self.genere)
-    
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-    
-    def __unicode__(self):
-        return u'%s\t%s\t%s\t%s\t%s\t%s\t%s' % (self.album,self.artist,self.composer,self.year,self.title,self.lyricist,self.genere)
-
 def run(directory):
+    setup_model()
     for root, _subFolders, files in os.walk(directory):
         for file in files:
             if file.endswith('.mp3'):
-                reader = MP3Reader(os.path.join(root,file))
-                print reader
+                try:
+                    _newtrack = Track.fromfile(os.path.join(root,file))
+                    print('Added: %s' % repr(_newtrack))
+                except FileExistsException as ex:
+                    print(ex.message)
                 
         
 
--- source/chakameh/model.py
+++ source/chakameh/model.py
@@ -4,17 +4,58 @@ Created on:    Dec 29, 2013
 @author:        vahid
 '''
 
-from elixir import Entity,metadata,Field,Unicode,Integer,UnicodeText
+from elixir import Entity,metadata,Field,Unicode,Integer,UnicodeText,session,setup_all,Binary
+import md5
+import id3reader
 
-metadata.bind = "sqlite:///tracks.sqlite"
-metadata.bind.echo = True
+metadata.bind = "sqlite:///../../data/tracks.sqlite"
+metadata.bind.echo = False
+
+class FileExistsException(Exception):
+    def __init__(self,filename):
+        Exception.__init__(self,'File already exists: %s' % filename)
 
 class Track(Entity):
     title = Field(Unicode(100))
-    performer = Field(Unicode(100))
+    artist = Field(Unicode(100))
     album = Field(Unicode(100))
+    composer = Field(Unicode(100))
     year = Field(Integer)
-    description = Field(UnicodeText)
+    lyricist = Field(Unicode(100))
+    genere = Field(Unicode(100))
+    comment = Field(UnicodeText)
+    filename = Field(Unicode(500)) # Relative to archive directpory 
+    filemd5 = Field(Binary(16),nullable=False,unique=True,index=True)
+    
+    @classmethod
+    def fromfile(cls,filename):
+        with open(filename) as f:
+            hash = md5.md5(f.read()).digest()
+        
+        if cls.query.filter(cls.filemd5 == hash).first():
+            raise FileExistsException(filename)
+        
+        reader = id3reader.MP3Reader(filename)
+        newtrack = cls()
+        newtrack.title = reader.title
+        newtrack.artist = reader.artist
+        newtrack.album = reader.album
+        newtrack.composer = reader.composer
+        newtrack.year = int(reader.year) if reader.year else None
+        newtrack.lyricist = reader.lyricist
+        newtrack.genere = reader.genere
+        newtrack.commebt = reader.comment
+        newtrack.filename = filename
+        newtrack.filemd5 = hash
+        
+        session.commit()
+        return newtrack
+        
     
     def __repr__(self):
-        return '<Track "%s" (%d)>' % (self.title, self.year)
+        return '<Track "%s" (%s)>' % (self.title, self.year)
+
+def setup():
+    setup_all(create_tables=True)
+
+
